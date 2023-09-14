989c38300536fa64bff861606eec77d5755fe045
DcGeminis <dcgeminishop2205@gmail.com>
2023-09-14 12:44:02 -0400

    Six

--- Models/Venta.cs
+++ Models/Venta.cs
@@ -8,24 +8,24 @@ namespace Lingzu.Models
 {
     public class Venta
     {
+        // Propiedad que representa el identificador único de la venta.
         public int VentaId { get; set; }
 
-        [Display(Name = "Fecha")]
-        [Required]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        // Propiedad que representa la fecha de la venta con validaciones.
+        [Display(Name = "Fecha")] // Etiqueta para mostrar en la interfaz de usuario.
+        [Required] // Campo obligatorio.
+        [DataType(DataType.Date)] // Tipo de datos para la fecha.
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] // Formato de visualización de la fecha.
         public DateTime Fecha { get; set; }
 
-        [Display(Name = "Número de Factura")]
-        [Required]
-        [StringLength(35, MinimumLength = 1)]
-        [RegularExpression(@"^\d+$")]
+        // Propiedad que representa el número de factura de la venta con validaciones.
+        [Display(Name = "Número de Factura")] // Etiqueta para mostrar en la interfaz de usuario.
+        [Required] // Campo obligatorio.
+        [StringLength(35, MinimumLength = 1)] // Longitud mínima y máxima permitida.
+        [RegularExpression(@"^\d+$")] // Expresión regular para validar que solo contiene dígitos.
         public string NumeroVenta { get; set; } = string.Empty;
 
-        
-      
-
-        //agregar propiedad de ralacion
+        // Propiedad que representa la relación con un cliente. El signo de interrogación indica que puede ser nulo.
         public Cliente? Clientes { get; set; }
     }
 }
