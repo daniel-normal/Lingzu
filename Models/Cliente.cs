a5e7a50a24f2fbf36c5cc62fce3c59541f0819ae
DcGeminis <dcgeminishop2205@gmail.com>
2023-09-14 11:42:11 -0400

    Fourth

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -8,32 +8,36 @@ namespace Lingzu.Models
 {
     public class Cliente
     {
-        //PROPIEDADES DEL MODELO CLIENTE
+        // Propiedad que representa el identificador único del cliente.
         public int ClienteId { get; set; }
-        //VALIDACIONES DE LA PROPIEDAD APELLIDO PATERNO
-        [Display(Name = "Apellido Paterno")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
-        [StringLength(35, MinimumLength = 1)]
-        [Required]
+
+        // Propiedad que representa el apellido paterno del cliente con validaciones.
+        [Display(Name = "Apellido Paterno")] // Etiqueta para mostrar en la interfaz de usuario.
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")] // Expresión regular para validar el formato.
+        [StringLength(35, MinimumLength = 1)] // Longitud mínima y máxima permitida.
+        [Required] // Campo obligatorio.
         public string ApellidoP { get; set; } = string.Empty;
-        //VALIDACIONES DE LA PROPIEDAD APELLIDO MATERNO
-        [Display(Name = "Apellido Materno")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
-        [StringLength(35, MinimumLength = 1)]
+
+        // Propiedad que representa el apellido materno del cliente con validaciones.
+        [Display(Name = "Apellido Materno")] // Etiqueta para mostrar en la interfaz de usuario.
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")] // Expresión regular para validar el formato.
+        [StringLength(35, MinimumLength = 1)] // Longitud mínima y máxima permitida.
         public string ApellidoM { get; set; } = string.Empty;
 
-        [Display(Name = "Nombre")]
-        [Required]
-        [StringLength(35, MinimumLength = 1)]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        // Propiedad que representa el nombre del cliente con validaciones.
+        [Display(Name = "Nombre")] // Etiqueta para mostrar en la interfaz de usuario.
+        [Required] // Campo obligatorio.
+        [StringLength(35, MinimumLength = 1)] // Longitud mínima y máxima permitida.
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")] // Expresión regular para validar el formato.
         public string Nombre { get; set; } = string.Empty;
 
-        [Display(Name = "NIT")]
-        [StringLength(35, MinimumLength = 4)]
-        [RegularExpression(@"^\d+$")]
+        // Propiedad que representa el NIT (Número de Identificación Tributaria) del cliente con validaciones.
+        [Display(Name = "NIT")] // Etiqueta para mostrar en la interfaz de usuario.
+        [StringLength(35, MinimumLength = 4)] // Longitud mínima y máxima permitida.
+        [RegularExpression(@"^\d+$")] // Expresión regular para validar que solo contiene dígitos.
         public string Nit { get; set; } = string.Empty;
 
-        // Agregar una colección de ventas relacionadas
+        // Propiedad que representa una colección de ventas relacionadas con este cliente.
         public ICollection<Venta> Ventas { get; set; } = new List<Venta>();
     }
 }
