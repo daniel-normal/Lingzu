39b52a0a92ec16f975aad85d4469457da67e81dd
daniel-normal <128575587+daniel-normal@users.noreply.github.com>
2023-09-14 00:34:04 -0400

    Agregar archivos de proyecto.

---
+++ Models/Producto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lingzu.Models
+{
+    public class Producto
+    {
+        public int ProductoId { get; set; }
+
+        [Display(Name = "Descripción")]
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        public string NombreProducto { get; set; } = string.Empty;
+
+        [Display(Name = "Precio")]
+        [Required]
+        [RegularExpression(@"^\d+$")]
+        public decimal PrecioProducto { get; set; }
+
+        [Display(Name = "Cantidad")]
+        [Required]
+        [RegularExpression(@"^\d+$")]
+        public int CantidadProducto { get; set; }
+
+        
+    }
+}
---
+++ Models/Venta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lingzu.Models
+{
+    public class Venta
+    {
+        public int VentaId { get; set; }
+
+        [Display(Name = "Fecha")]
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha { get; set; }
+
+        [Display(Name = "Número de Factura")]
+        [Required]
+        [StringLength(35, MinimumLength = 1)]
+        [RegularExpression(@"^\d+$")]
+        public string NumeroVenta { get; set; } = string.Empty;
+
+        
+      
+
+        //agregar propiedad de ralacion
+        public Cliente? Clientes { get; set; }
+    }
+}
