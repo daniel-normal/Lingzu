8cc245794734eca599f5a6a268383a039fd4f0cc
DcGeminis <dcgeminishop2205@gmail.com>
2023-09-14 12:37:28 -0400

    Five

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -8,23 +8,27 @@ namespace Lingzu.Models
 {
     public class Producto
     {
+        // Propiedad que representa el identificador único del producto.
         public int ProductoId { get; set; }
 
-        [Display(Name = "Descripción")]
-        [Required]
-        [StringLength(100, MinimumLength = 1)]
+        // Propiedad que representa el nombre o descripción del producto con validaciones.
+        [Display(Name = "Descripción")] // Etiqueta para mostrar en la interfaz de usuario.
+        [Required] // Campo obligatorio.
+        [StringLength(100, MinimumLength = 1)] // Longitud mínima y máxima permitida.
         public string NombreProducto { get; set; } = string.Empty;
 
-        [Display(Name = "Precio")]
-        [Required]
-        [RegularExpression(@"^\d+$")]
+        // Propiedad que representa el precio del producto con validaciones.
+        [Display(Name = "Precio")] // Etiqueta para mostrar en la interfaz de usuario.
+        [Required] // Campo obligatorio.
+        [RegularExpression(@"^\d+$")] // Expresión regular para validar que solo contiene dígitos.
         public decimal PrecioProducto { get; set; }
 
-        [Display(Name = "Cantidad")]
-        [Required]
-        [RegularExpression(@"^\d+$")]
+        // Propiedad que representa la cantidad disponible del producto con validaciones.
+        [Display(Name = "Cantidad")] // Etiqueta para mostrar en la interfaz de usuario.
+        [Required] // Campo obligatorio.
+        [RegularExpression(@"^\d+$")] // Expresión regular para validar que solo contiene dígitos.
         public int CantidadProducto { get; set; }
 
-        
+
     }
 }
